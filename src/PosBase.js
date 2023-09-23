618a23b51ba7131ab0cfcafcd5632a1eca0954f9
Mauricio Astudillo <m.astudillo.toledo@gmail.com>
2023-09-22 23:50:39 -0300

    refactor: adapt code to updated serialport library

--- src/PosBase.js
+++ src/PosBase.js
@@ -1,7 +1,7 @@
 const LRC = require("lrc-calculator")
-const SerialPort = require("serialport")
+const SerialPort = require("serialport").SerialPort;
 const EventEmitter = require('events');
-const InterByteTimeout = require("@serialport/parser-inter-byte-timeout")
+const InterByteTimeout = require("@serialport/parser-inter-byte-timeout").InterByteTimeoutParser;
 const responseMessages = require("./responseCodes");
 const ACK = 0x06
 
@@ -102,7 +102,11 @@ module.exports = class POSBase extends EventEmitter {
 
             this.connecting = true
 
-            this.port = new SerialPort(portName, { baudRate, autoOpen: false })
+            this.port = new SerialPort({
+                path: portName,
+                baudRate: baudRate,
+                autoOpen: false
+            })
 
             this.port.open((err) => {
                 if (err) {
