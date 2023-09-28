aec22fbcb4cd223bf48a99f14eb8fec4a512a406
Mauricio Astudillo <m.astudillo.toledo@gmail.com>
2023-09-28 18:06:02 -0300

    refactor: remove comments from example index

--- examples/index.js
+++ examples/index.js
@@ -1,21 +1,20 @@
 const { POSAutoservicio, POSIntegrado } = require('../index')
 
-const pos = new POSAutoservicio();
-// const pos = new POSIntegrado();
+const pos = new POSIntegrado();
 
     pos.setDebug(true);
     pos.autoconnect().then(port => {
         if (port) {
             console.log('Connected to', port.path)
         };
-        // pos.loadKeys().then(response => console.log(response))
-        // pos.sale(1000, Math.floor(Math.random() * 10000) + 1, true).then(response => console.log(response)).catch(error => console.log(error))
-        // pos.sale(1000, Math.floor(Math.random() * 10000) + 1, true, true).then(response => console.log(response)).catch(error => console.log(error))
+        pos.loadKeys().then(response => console.log(response))
+        pos.sale(1000, Math.floor(Math.random() * 10000) + 1, true).then(response => console.log(response)).catch(error => console.log(error))
+        pos.sale(1000, Math.floor(Math.random() * 10000) + 1, true, true).then(response => console.log(response)).catch(error => console.log(error))
         pos.refund().then(data => console.log(data)).catch(error => console.log(error))
-        // pos.salesDetail(false).then(result => console.log(result))
-        // pos.closeDay()
-        // pos.loadKeys().then(response => {
-        //     console.log(response)
-        //     pos.closeDay().then(response => console.log(response))
-        // })
+        pos.salesDetail(false).then(result => console.log(result))
+        pos.closeDay()
+        pos.loadKeys().then(response => {
+            console.log(response)
+            pos.closeDay().then(response => console.log(response))
+        })
     }).catch(error => console.log(error));
