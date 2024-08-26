79d5e77eac8042f07c6da326da55b46e161a10bc
Mauricio Astudillo Toledo <m.astudillo.toledo@gmail.com>
2024-08-26 14:06:01 -0400

    feat: update export from POS implementations

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
-const PosIntegrado = require('./src/PosIntegrado');
-const PosAutoservicio = require('./src/PosAutoservicio')
+const POSIntegrado = require("./src/PosIntegrado");
+const POSAutoservicio = require("./src/PosAutoservicio");
 
-const Transbank = {};
-Transbank.POSIntegrado = PosIntegrado;
-Transbank.POSAutoservicio = PosAutoservicio;
+const Transbank = {
+  POSIntegrado,
+  POSAutoservicio,
+};
 
 module.exports = Transbank;
