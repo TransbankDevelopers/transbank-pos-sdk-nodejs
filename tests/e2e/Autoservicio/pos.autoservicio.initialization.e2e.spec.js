497fbf553ead0f2290b9f4319fad37d23f5ca14e
Matias Vasquez <m.vasquezac@gmail.com>
2026-03-26 17:15:06 -0300

    test: add initialization test

---
+++ tests/e2e/Autoservicio/pos.autoservicio.initialization.e2e.spec.js
@@ -0,0 +1,49 @@
+const { setupSuiteContext } = require("../helpers/suiteContext");
+const {
+    ACK_BYTE,
+    buildMessage,
+    captureSend,
+    sendReply,
+    connectWithPollAck,
+    expectResponseFields
+} = require("../helpers/mockPos");
+
+const createConnectedPos = async (suite) => {
+    const pos = suite.createAutoservicio();
+    await connectWithPollAck(pos);
+    return pos;
+}
+
+describe("POS Autoservicio - Inicialización", () => {
+    const suite =setupSuiteContext();
+
+    it("inicializa el pos", async () => {
+        const pos = await createConnectedPos(suite);
+
+        const promise = pos.initialization();
+        const sentMessage = await captureSend(pos);
+        await sendReply(pos, ACK_BYTE);
+        expect(sentMessage).toEqual(buildMessage("0070"));
+        await expect(promise).resolves.toBe(true);
+    });
+
+    it("parsea respuesta de inicializacion", async () => {
+        const pos = await createConnectedPos(suite);
+
+        const promise = pos.initializationResponse();
+        const sentMessage = await captureSend(pos);
+        await sendReply(pos, ACK_BYTE);
+        await sendReply(pos, "1080|90|16022026|103654");
+        const response = await promise;
+        
+        expect(sentMessage).toEqual(buildMessage("0080"));
+        expectResponseFields(response, {
+            functionCode: 1080,
+            responseCode: 90,
+            responseMessage: "Inicialización exitosa",
+            transactionDate: "16022026",
+            transactionTime: "103654",
+            successful: true
+        });
+    });
+});
