1424280ee256724b71c4574943263a772db6194b
Matias Vasquez <m.vasquezac@gmail.com>
2026-03-26 17:14:22 -0300

    test: add poll test

---
+++ tests/e2e/Autoservicio/pos.autoservicio.poll.e2e.spec.js
@@ -0,0 +1,35 @@
+const { setupSuiteContext } = require("../helpers/suiteContext");
+const {
+    ACK_BYTE,
+    buildMessage,
+    captureSend,
+    sendReply,
+    connectWithPollAck,
+} = require("../helpers/mockPos");
+
+const createConnectedPos = async (suite) => {
+    const pos = suite.createAutoservicio();
+    await connectWithPollAck(pos);
+    return pos;
+}
+
+describe("POS Autoservicio - Poll", () => {
+    const suite =setupSuiteContext();
+
+    it("Comprueba conexión con resultado ok", async () => {
+        const pos = await createConnectedPos(suite);
+        
+        const pollPromise = pos.poll();
+        const sentMessage = await captureSend(pos);
+        await sendReply(pos, ACK_BYTE);
+        expect(sentMessage).toEqual(buildMessage("0100"));
+        const response = await pollPromise;
+        expect(response).toBe(true);
+    });
+
+    it("Comprueba conexión con resultado error", async () => {
+        const pos = suite.createAutoservicio();
+
+        await expect(pos.poll()).rejects.toThrow("You have to connect to a POS");
+    });
+});
