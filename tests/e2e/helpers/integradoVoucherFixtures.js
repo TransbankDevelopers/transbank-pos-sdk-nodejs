99f33a60c6fd3ce09cd7f69a88a1b08464b1480f
Matias Vasquez <m.vasquezac@gmail.com>
2026-05-08 09:47:39 -0400

    test: add voucher examples

---
+++ tests/e2e/helpers/integradoVoucherFixtures.js
@@ -0,0 +1,150 @@
+const multiCodeSaleCreditVoucher =
+    "               TRANSBANK                " +
+    "     VENTA CON PIN - COPIA COMERCIO     " +
+    "           TARJETA DE CREDITO           " +
+    "              PRUEBA DESA               " +
+    "             Cerro El Plomo             " +
+    "           CIUDAD DESARROLLO            " +
+    "     597029414300-IT750050-I25.0A0      " +
+    "              597029414303              " +
+    "06/04/2026      23:41:09                " +
+    "                           ****6590 B-VI" +
+    "MONTO VENTA:                     $10.084" +
+    "IVA:                              $1.916" +
+    "TOTAL:                           $12.000" +
+    "3 CUOTAS SIN INTERES TASA:0,00%         " +
+    "MONTO CUOTA:                      $4.000" +
+    "MONEDA:                             PESO" +
+    "OPERACION: 000141   AUTORIZACION: 794160" +
+    "                                        " +
+    "                                        " +
+    "                                        " +
+    "                                        " +
+    "                                        " +
+    "                                        " +
+    "               TRANSBANK                " +
+    "     VENTA CON PIN - COPIA CLIENTE      " +
+    "           TARJETA DE CREDITO           " +
+    "              PRUEBA DESA               " +
+    "             Cerro El Plomo             " +
+    "           CIUDAD DESARROLLO            " +
+    "     597029414300-IT750050-I25.0A0      " +
+    "              597029414303              " +
+    "06/04/2026      23:41:09                " +
+    "                           ****6590 B-VI" +
+    "MONTO VENTA:                     $10.084" +
+    "IVA:                              $1.916" +
+    "TOTAL:                           $12.000" +
+    "3 CUOTAS SIN INTERES TASA:0,00%         " +
+    "MONTO CUOTA:                      $4.000" +
+    "MONEDA:                             PESO" +
+    "OPERACION: 000141   AUTORIZACION: 794160" +
+    "                                        " +
+    "                                        " +
+    "                                        " +
+    "                                        " +
+    "                                        ";
+
+const multiCodeSaleDebitVoucher =
+    "               TRANSBANK                " +
+    "         VENTA - COPIA COMERCIO         " +
+    "           TARJETA DE DEBITO            " +
+    "              PRUEBA DESA               " +
+    "             Cerro El Plomo             " +
+    "           CIUDAD DESARROLLO            " +
+    "     597029414300-IT750050-I25.0A0      " +
+    "              597029414303              " +
+    "07/04/2026      08:50:34                " +
+    "                           ****3331 B-DB" +
+    "MONTO VENTA:                      $6.723" +
+    "IVA:                              $1.277" +
+    "TOTAL:                            $8.000" +
+    "MONEDA:                             PESO" +
+    "OPERACION: 000143   AUTORIZACION: 708410" +
+    "                                        " +
+    "                                        " +
+    "                                        " +
+    "                                        " +
+    "                                        " +
+    "                                        " +
+    "               TRANSBANK                " +
+    "         VENTA - COPIA CLIENTE          " +
+    "           TARJETA DE DEBITO            " +
+    "              PRUEBA DESA               " +
+    "             Cerro El Plomo             " +
+    "           CIUDAD DESARROLLO            " +
+    "     597029414300-IT750050-I25.0A0      " +
+    "              597029414303              " +
+    "07/04/2026      08:50:34                " +
+    "                           ****3331 B-DB" +
+    "MONTO VENTA:                      $6.723" +
+    "IVA:                              $1.277" +
+    "TOTAL:                            $8.000" +
+    "MONEDA:                             PESO" +
+    "OPERACION: 000143   AUTORIZACION: 708410" +
+    "                                        " +
+    "                                        " +
+    "                                        " +
+    "                                        " +
+    "                                        " +
+    "                                        ";
+
+const normalSaleCreditVoucher =
+    "               TRANSBANK                " +
+    "         VENTA - COPIA COMERCIO         " +
+    "           TARJETA DE CREDITO           " +
+    "              PRUEBA DESA               " +
+    "              Razon Social              " +
+    "             Cerro El Plomo             " +
+    "           CIUDAD DESARROLLO            " +
+    "           RUT: 11.111.111-1            " +
+    "     597029414300-IT750870-I25.0A0      " +
+    "           VALIDO COMO BOLETA           " +
+    "07/05/2026      16:11:40                " +
+    "                           ****6590 B-VI" +
+    "MONTO VENTA:                     $32.773" +
+    "IVA:                              $6.227" +
+    "TOTAL:                           $39.000" +
+    "MONEDA:                             PESO" +
+    "OPERACION: 000156   AUTORIZACION: 532264" +
+    "                                        " +
+    "FIRMA:__________________________________" +
+    "__                                      " +
+    "SR(A) PRUEBAS/VISA008                   " +
+    "                                        " +
+    "CI :____________________________________" +
+    "                                        " +
+    "                                        " +
+    "                                        " +
+    "                                        " +
+    "                                        " +
+    "                                        " +
+    "               TRANSBANK                " +
+    "         VENTA - COPIA CLIENTE          " +
+    "           TARJETA DE CREDITO           " +
+    "              PRUEBA DESA               " +
+    "              Razon Social              " +
+    "             Cerro El Plomo             " +
+    "           CIUDAD DESARROLLO            " +
+    "           RUT: 11.111.111-1            " +
+    "     597029414300-IT750870-I25.0A0      " +
+    "           VALIDO COMO BOLETA           " +
+    "07/05/2026      16:11:40                " +
+    "                           ****6590 B-VI" +
+    "MONTO VENTA:                     $32.773" +
+    "IVA:                              $6.227" +
+    "TOTAL:                           $39.000" +
+    "MONEDA:                             PESO" +
+    "OPERACION: 000156   AUTORIZACION: 532264" +
+    "                                        " +
+    "                                        " +
+    "                                        " +
+    "                                        " +
+    "                                        " +
+    "                                        ";
+
+module.exports = {
+    multiCodeSaleCreditVoucher,
+    multiCodeSaleDebitVoucher,
+    normalSaleCreditVoucher
+};
